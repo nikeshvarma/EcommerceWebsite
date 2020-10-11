01521f67bcaced16167dc9f4e34c83dfbe28402f
Nikesh <nikeshvarma50@gmail.com>
2020-10-11 23:27:44 +0530

    1.1

--- HOME/urls.py
+++ HOME/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home_page'),
-    path('/contact-us', views.ContactView.as_view(), name='contact_us'),
-    path('/about-us', views.AboutView.as_view(), name='about_us'),
+    path('contact-us', views.ContactView.as_view(), name='contact_us'),
+    path('about-us', views.AboutView.as_view(), name='about_us'),
 ]
